7683565d4f888bda0de80a2638f50115d4b2324e
Awni <Wintrygamer@gmail.com>
2018-03-20 00:53:38 +0100

    Improved

--- src/Awni/Controllers/Main.php
+++ src/Awni/Controllers/Main.php
@@ -23,20 +23,6 @@ class Main extends Controller
      */
     public function home(Request $request, Response $response)
     {
-        $apikey = "cc12540abedfa669021307d4ba111d87";
-        $bdb = new \Pintlabs_Service_Brewerydb($apikey);
-        $bdb->setFormat('php'); // if you want to get php back.  'xml' and 'json' are also valid options.
-        $params = array("q" => "lol", "type" => "beer");
-
-        try {
-            // The first argument to request() is the endpoint you want to call
-            // 'brewery/BrvKTz', 'beers', etc.
-            // The third parameter is the HTTP method to use (GET, PUT, POST, or DELETE)
-            $results = $bdb->request('beers', $params, 'GET'); // where $params is a keyed array of parameters to send with the API call.
-        } catch (Exception $e) {
-            $results = array('error' => $e->getMessage());
-        }
-
         return $this->container['theme']
             ->with([
                 'content' => View::make('page/home'),
@@ -63,9 +49,38 @@ class Main extends Controller
 
     public function searchProccess(Request $request, Response $response)
     {
+        $postVar = $request->getParsedbody();
+        $text = $postVar['search'];
+
+        $apikey = "cc12540abedfa669021307d4ba111d87";
+        $bdb = new \Pintlabs_Service_Brewerydb($apikey);
+        $bdb->setFormat('php'); // if you want to get php back.  'xml' and 'json' are also valid options.
+        $params = array("tes" => $text, "type" => "beer");
+
+        try {
+            $results = $bdb->request('beers', $params, 'GET'); // where $params is a keyed array of parameters to send with the API call.
+        } catch (Exception $e) {
+            $results = array('error' => $e->getMessage());
+        }
+
+        //$newResults = $results['data'];
+        $size = sizeof($results['data']);
+        $arr = array();
+        $params = array();
+
+        for($i = 0; $i < 5; $i++) {
+
+            try {
+                $arr[$i] = $bdb->request('beer/' . $results['data'][$i]['id'], $params, 'GET'); // where $params is a keyed array of parameters to send with the API call.
+            } catch (Exception $e) {
+                $arr[$i] = array('error' => $e->getMessage());
+            }
+        }
+
+
         return $this->container['theme']
             ->with([
-                'content' => View::make('page/home')->with(['test' => 'teeeeeeeests']),
+                'content' => View::make('page/home')->with(['results' => $arr]),
                 'page' => 'Home',
             ])
             ->renderInto($response);
