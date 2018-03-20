cc24fa3f8a187947ec41d0774f50733bd50e5a6f
Awni <Wintrygamer@gmail.com>
2018-03-20 02:15:56 +0100

    Save

--- src/Awni/Controllers/Main.php
+++ src/Awni/Controllers/Main.php
@@ -77,13 +77,15 @@ class Main extends Controller
     {
         $postVar = $request->getParsedbody();
 
+        $beerId = explode('/', $request->getAttribute('id'))[0];
+        $apikey = "cc12540abedfa669021307d4ba111d87";
         $bdb = new \Pintlabs_Service_Brewerydb($apikey);
         $bdb->setFormat('php'); // if you want to get php back.  'xml' and 'json' are also valid options.
         $params = array();
         $results = "";
 
         try {
-            $results = $bdb->request('search', $params, 'GET'); // where $params is a keyed array of parameters to send with the API call.
+            $results = $bdb->request('beer/' . $beerId, $params, 'GET'); // where $params is a keyed array of parameters to send with the API call.
         } catch (Exception $e) {
             $results = array('error' => $e->getMessage());
         }
