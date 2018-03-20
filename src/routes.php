90e60be0b943fced60dd74cd3c8910e4e76ddf3c
Awni <Wintrygamer@gmail.com>
2018-03-20 02:28:07 +0100

    test

--- src/routes.php
+++ src/routes.php
@@ -5,8 +5,8 @@ use Awni\Middlewares\Middleware;
 
 $app->get('/', Main::class . ':home');
 $app->get('/home', Main::class . ':home');
+$app->get('/home/id/{id}', Main::class . ':item');
 $app->get('/search', Main::class . ':search');
 $app->get('/about', Main::class . ':about');
 $app->post('/', Main::class . ':searchProccess');
 $app->post('/home', Main::class . ':searchProccess');
-$app->get('/id/{id}', Main::class . ':item');
